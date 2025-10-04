09b34dd9626abd000c4365f72c7eb6f95cb973e6
KwanJunEr <jonastheitguy@gmail.com>
2025-10-04 15:45:08 +0800

    commit files for html and css building and also database connections

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+
+//1. Connect to Local MYSQL Sever (using XAMPP or MAMPP)
+$username = "root";
+$conn = new mysqli("localhost", $username, "","course_calendar");
+$conn->set_charset("utf8mb4");
+
+?>
